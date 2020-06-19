4e751b71d70c7acb420e1e5de238dabfa217590a
Aleksey <Arhangelruh@gmail.com>
2020-06-19 09:42:32 +0300

    feat: adding method with arrays

--- src/Homework1/Program.cs
+++ src/Homework1/Program.cs
@@ -18,10 +18,8 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-
-             
               Console.WriteLine("Enter the day number (1-7) of the week and we will say what day it is: ");
-
+            string[] weekDays = { "Monday", "Tuesday", "Wednasday", "Thursday", "Friday", "Saturday", "Sunday"};
             try
             {
                 int key = int.Parse(Console.ReadLine());
@@ -31,18 +29,22 @@ namespace Homework1
                     var dayOfWeek = (Week)key;
 
                     Console.WriteLine("The " + key + " day of week it is: " + dayOfWeek);
+                    Console.WriteLine("\r\nSame with arroy:");
+                    var dayOfWeek2 = key-1 ;
+                    Console.WriteLine("The " + key + " day of week it is: "+weekDays[dayOfWeek2]);
                 }
                 else
                 {
                     Console.WriteLine("In week only seven days  :*( ");
                 }
-
-
                 
+
+
             }
             catch {
                 Console.WriteLine("The entered value is not a number");
             }
+            
             Console.ReadKey();
         }
     }
