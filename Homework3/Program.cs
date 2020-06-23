5e0485a07ceb7584ff83ed12943d51ec7f6605ef
Aleksey <Arhangelruh@gmail.com>
2020-06-23 17:36:27 +0300

    init: task list from the day

---
+++ Homework3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> tasksName = new List<string>();
+            List<string> timeName = new List<string>();
+            string question;
+           
+            do
+            {
+                Console.WriteLine("\nInsert task: ");
+                string task = Console.ReadLine();
+                tasksName.Add(task);
+                Console.WriteLine("\nInsert time for your task: ");
+                string time = Console.ReadLine();
+                timeName.Add(time);
+                Console.WriteLine("\nWant to enter another task?(Press eny key or n)");
+                string q = Console.ReadLine();
+                question = q.ToUpper();
+            }
+            while (question != "N");
+
+            for (var i = 0; i < tasksName.Count && i < timeName.Count; i++)
+            {
+                string a = tasksName[i];
+                string b = timeName[i];
+                Task task = new Task(a, b);
+                task.Show();
+            }
+                Console.ReadKey();
+        }
+    }
+}
