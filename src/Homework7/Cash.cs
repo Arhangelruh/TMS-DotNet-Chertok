e3debeea196b5a7e9ddd6ace30ab624750ea08c4
Aleksey <arhangelruh@gmail.com>
2020-07-15 10:01:48 +0300

    feat: add randome speed work for cash

---
+++ src/Homework7/Cash.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework7
+{
+    class Cash {
+        private static Random _random = new Random();
+        private static int SpeedWork = _random.Next(500, 1500);
+
+        public static void WorkCash(object cash)
+        {
+            DateTime timework = DateTime.Now;
+            Random random = new Random();
+            int timeopen = 10;
+            int timeclose = 56;
+
+            if (timework.Minute > timeopen && timework.Minute < timeclose)
+            {
+                for (int i = 1; timework.Minute < timeclose; i++)
+                {
+                    timework = DateTime.Now;
+                    var purchase = random.Next(1, 50);
+                    for (int j = 1; j <= purchase; j++)
+                    {
+                        Console.WriteLine($"Cash №{(int)cash} Clients {i} Purchase {j}");
+                        Thread.Sleep(SpeedWork);
+                    }
+                };
+                Console.WriteLine("The cash is closed");
+            }
+            else
+            {
+
+                Console.WriteLine("The store is closed");
+            }
+        }
+    }
+}
--- src/Homework7/Program.cs
+++ src/Homework7/Program.cs
@@ -8,41 +8,12 @@ namespace Homework7
         
         static void Main(string[] args)
         {
-
             for (int i = 1; i <= 5; i++)
             {
-                Thread thread = new Thread(WorkCash);
+                Thread thread = new Thread(Cash.WorkCash);
                 thread.Start(i);
             }
             Console.ReadKey();
-        }
-
-        public static void WorkCash(object cash)
-        {
-            DateTime timework = DateTime.Now;
-            Random random = new Random();
-            int timeopen = 10;
-            int timeclose = 23;
-
-            if (timework.Minute > timeopen && timework.Minute < timeclose)
-            {
-                for (int i = 1; timework.Minute < timeclose ; i++)
-                {
-                    timework = DateTime.Now;
-                    var purchase = random.Next(1, 50);
-                    for (int j = 1; j <= purchase; j++)
-                    {
-                        Console.WriteLine($"Cash №{(int)cash} Clients {i} Purchase {j}");
-                        Thread.Sleep(1000);
-                    }
-                };
-                Console.WriteLine("The cash is closed");
-            }
-            else
-            {
-
-                Console.WriteLine("The store is closed");
-            }
         }
     }
 }
